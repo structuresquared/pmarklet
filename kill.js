933c469d5f1488a77559d1a9acd1e721a300d233
Eric Schiller <ericschiller@gmail.com>
2011-12-15 16:44:53 -0800

    initial commit

--- kill.js
+++
@@ -1,8 +0,0 @@
-var isEnabled = true;
-console.log("isEnabled status " + isEnabled);
-if (isEnabled === true) {
-  $(document).ready(function(){
-    $('contentArea').append('<br />News feed disabled by Feed Frack.');
-  });
-
-}; 
